8a5d19031af6031fa30d7ee40093599082fe4b8e
David Rincón <david.rincon@prototipo0.com>
2024-02-15 23:02:02 +0100

    servicio login

---
+++ ConsumirSaml2Sustainsys/Controllers/LoginIdp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumirSaml2Sustainsys.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class LoginIdp:Microsoft.AspNetCore.Mvc.ControllerBase
+    {        
+        [Microsoft.AspNetCore.Mvc.HttpGet(Name = "login-idp")]
+        public async Task<IActionResult> LoginAdp() {            
+            return Ok("Login satisfactorio");
+        }
+
+    }
+}
